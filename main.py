d6c8ad03f1ca087dd38533214ee7d12fb6d41649
Phantichchai Kharnboriwarn <phantichchai1998@gmail.com>
2024-01-28 21:45:33 +0700

    fix conflict with stash

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ import chainlit as cl
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
-<<<<<<< Updated upstream
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores.faiss import FAISS
-from langchain.chains import RetrievalQA
-=======
 from chain import runnable
->>>>>>> Stashed changes
 
 
 prompt = ChatPromptTemplate.from_messages(
@@ -18,38 +12,6 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-<<<<<<< Updated upstream
-def load_llm():
-    llm = CTransformers(
-        model="model/llama-2-7b-chat.Q6_K.gguf",
-        model_type="llama",
-        max_new_tokens=100,
-        context_length=4096,
-        temperature=0.5,
-        device=device,
-        stop=["Question:", "\n"]
-    )
-    return llm
-
-def qa_bot():
-    embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2',
-                                        model_kwargs= {'device': device})
-    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
-    llm = load_llm()
-    qa_prompt = set_custom_prompt()
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=db.as_retriever(search_kwargs={'k': 3}),
-        return_source_documents=True,
-        chain_type_kwargs= {'prompt': qa_prompt}
-    )
-
-    return qa
-
-    
-=======
->>>>>>> Stashed changes
 ## Chainlit ##
 @cl.on_chat_start
 async def start():
