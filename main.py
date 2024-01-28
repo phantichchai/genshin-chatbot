d6b9d3ec74df3acae69e23778e5baa95ebd26194
Phantichchai Kharnboriwarn <phantichchai1998@gmail.com>
2024-01-28 22:10:52 +0700

    remove unused code

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ import chainlit as cl
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import Runnable
 from langchain.schema.runnable.config import RunnableConfig
-<<<<<<< HEAD
 from chain import runnable
-=======
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.vectorstores.faiss import FAISS
-from langchain.chains import RetrievalQA
->>>>>>> main
 
 
 prompt = ChatPromptTemplate.from_messages(
@@ -18,47 +12,6 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-<<<<<<< HEAD
-=======
-def set_custom_prompt():
-    """
-    Prompt template for QA retrieval for each vector stores
-    """
-
-    prompt = PromptTemplate(template=custom_prompt_template, input_variables=['context', 'question'])
-
-    return prompt
-
-def load_llm():
-    llm = CTransformers(
-        model="model/llama-2-7b-chat.Q6_K.gguf",
-        model_type="llama",
-        max_new_tokens=1024,
-        context_length=4096,
-        temperature=0.5,
-        device=device,
-        stop=["Question:", "\n"]
-    )
-    return llm
-
-def qa_bot():
-    embeddings = HuggingFaceEmbeddings(model_name = 'sentence-transformers/all-MiniLM-L6-v2',
-                                        model_kwargs= {'device': device})
-    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
-    llm = load_llm()
-    qa_prompt = set_custom_prompt()
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="stuff",
-        retriever=db.as_retriever(search_kwargs={'k': 3}),
-        return_source_documents=True,
-        chain_type_kwargs= {'prompt': qa_prompt}
-    )
-
-    return qa
-
-    
->>>>>>> main
 ## Chainlit ##
 @cl.on_chat_start
 async def start():
